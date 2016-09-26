b6e1a437e6a9a5d17ef04a6214fcb455154b24e2
shift02 <shift02ss@gmail.com>
2016-09-26 22:50:48 +0900

    追加：Jsonでデータの管理

--- gulp/tasks/buildEJS.js
+++ gulp/tasks/buildEJS.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var ejs = require("gulp-ejs");
+var fs =require('fs')
 
 gulp.task('buildEJS', function() {
 
+    var jsonData =  JSON.parse(fs.readFileSync('./src/main/ejs/data.json', 'utf8'));
+
     var ejs_src = ["./src/main/ejs/**/*.ejs", "!./src/main/ejs/**/_*.ejs"];
 	gulp.src(ejs_src)
-	.pipe(ejs('',{"ext": ".html"}))
+	.pipe(ejs({jsonData: jsonData},{"ext": ".html"}))
 	.pipe(gulp.dest("./"));
 
 });
